bd338733163f11833d496db9620e6f952f5ff667
Sarthak Jha <sarthak333jha@gmail.com>
2022-05-22 00:11:47 +0530

    chore: poc fin

---
+++ lib/main.dart
@@ -0,0 +1,335 @@
+import 'dart:convert';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/services.dart';
+
+enum Direction { up, down, right, left, none }
+
+void main() {
+  MyGame game = MyGame();
+  runApp(MaterialApp(
+    home: Stack(
+      children: [
+        GameWidget(game: game),
+        Joypad(
+          onDirectionChanged: (direction) =>
+              game.onJoypadDirectionChanged(direction),
+        ),
+      ],
+    ),
+  ));
+}
+
+class MyGame extends FlameGame with HasCollisionDetection {
+  final Player _player = Player();
+  final World _world = World();
+
+  @override
+  Future<void> onLoad() async {
+    add(_world);
+    add(_player);
+    _player.position = Vector2(500, 500);
+    camera.followComponent(
+      _player,
+    );
+    addWorldCollision();
+  }
+
+  void onJoypadDirectionChanged(Direction direction) {
+    _player.direction = direction;
+  }
+
+  void addWorldCollision() async =>
+      (await MapLoader.readRayWorldCollisionMap()).forEach((rect) {
+        add(WorldCollidable()
+          ..position = Vector2(rect.left, rect.top)
+          ..width = rect.width
+          ..height = rect.height);
+      });
+}
+
+class Player extends SpriteAnimationComponent
+    with HasGameRef, CollisionCallbacks {
+  Player()
+      : super(
+          size: Vector2.all(50.0),
+        ) {
+    add(RectangleHitbox());
+  }
+
+  Direction direction = Direction.none;
+  final double _animationSpeed = 0.15;
+  late final SpriteAnimation _runDownAnimation;
+  late final SpriteAnimation _runLeftAnimation;
+  late final SpriteAnimation _runUpAnimation;
+  late final SpriteAnimation _runRightAnimation;
+  late final SpriteAnimation _standingAnimation;
+  final double _playerSpeed = 300.0;
+  bool _hasCollided = false;
+  Direction _collisionDirection = Direction.none;
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    _loadAnimations().then((_) => {animation = _standingAnimation});
+  }
+
+  @override
+  void onCollision(Set<Vector2> points, PositionComponent other) {
+    if (other is WorldCollidable) {
+      if (!_hasCollided) {
+        _hasCollided = true;
+        _collisionDirection = direction;
+      }
+    }
+  }
+
+  @override
+  void onCollisionEnd(PositionComponent other) {
+    _hasCollided = false;
+  }
+
+  Future<void> _loadAnimations() async {
+    final spriteSheet = SpriteSheet(
+      image: await gameRef.images.load('player_spritesheet.png'),
+      srcSize: Vector2(29.0, 32.0),
+    );
+
+    _runDownAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed, to: 4);
+    _runLeftAnimation =
+        spriteSheet.createAnimation(row: 1, stepTime: _animationSpeed, to: 4);
+
+    _runUpAnimation =
+        spriteSheet.createAnimation(row: 2, stepTime: _animationSpeed, to: 4);
+
+    _runRightAnimation =
+        spriteSheet.createAnimation(row: 3, stepTime: _animationSpeed, to: 4);
+
+    _standingAnimation =
+        spriteSheet.createAnimation(row: 0, stepTime: _animationSpeed, to: 1);
+  }
+
+  @override
+  void update(double delta) {
+    super.update(delta);
+    movePlayer(delta);
+  }
+
+  bool canPlayerMoveUp() {
+    if (_hasCollided && _collisionDirection == Direction.up) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveDown() {
+    if (_hasCollided && _collisionDirection == Direction.down) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveLeft() {
+    if (_hasCollided && _collisionDirection == Direction.left) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveRight() {
+    if (_hasCollided && _collisionDirection == Direction.right) {
+      return false;
+    }
+    return true;
+  }
+
+  void movePlayer(double delta) {
+    switch (direction) {
+      case Direction.up:
+        if (canPlayerMoveUp()) {
+          animation = _runUpAnimation;
+          moveUp(delta);
+        }
+        break;
+      case Direction.down:
+        if (canPlayerMoveDown()) {
+          animation = _runDownAnimation;
+          moveDown(delta);
+        }
+        break;
+      case Direction.left:
+        if (canPlayerMoveLeft()) {
+          animation = _runLeftAnimation;
+          moveLeft(delta);
+        }
+        break;
+      case Direction.right:
+        if (canPlayerMoveRight()) {
+          animation = _runRightAnimation;
+          moveRight(delta);
+        }
+        break;
+      case Direction.none:
+        animation = _standingAnimation;
+        break;
+    }
+  }
+
+  void moveDown(double delta) {
+    position.add(Vector2(0, delta * _playerSpeed));
+  }
+
+  void moveUp(double delta) {
+    position.add(Vector2(0, delta * -_playerSpeed));
+  }
+
+  void moveLeft(double delta) {
+    position.add(Vector2(delta * -_playerSpeed, 0));
+  }
+
+  void moveRight(double delta) {
+    position.add(Vector2(delta * _playerSpeed, 0));
+  }
+}
+
+class World extends SpriteComponent with HasGameRef {
+  @override
+  Future<void>? onLoad() async {
+    sprite = await gameRef.loadSprite('rayworld_background.png');
+    size = sprite!.originalSize;
+    return super.onLoad();
+  }
+}
+
+class Joypad extends StatefulWidget {
+  final ValueChanged<Direction>? onDirectionChanged;
+
+  const Joypad({Key? key, this.onDirectionChanged}) : super(key: key);
+
+  @override
+  JoypadState createState() => JoypadState();
+}
+
+class JoypadState extends State<Joypad> {
+  Direction direction = Direction.none;
+  Offset delta = Offset.zero;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      width: 120,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(60),
+        ),
+        child: GestureDetector(
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color(0x88ffffff),
+              borderRadius: BorderRadius.circular(60),
+            ),
+            child: Center(
+              child: Transform.translate(
+                offset: delta,
+                child: SizedBox(
+                  height: 60,
+                  width: 60,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xccffffff),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          onPanDown: onDragDown,
+          onPanUpdate: onDragUpdate,
+          onPanEnd: onDragEnd,
+        ),
+      ),
+    );
+  }
+
+  void updateDelta(Offset newDelta) {
+    final newDirection = getDirectionFromOffset(newDelta);
+
+    if (newDirection != direction) {
+      direction = newDirection;
+      widget.onDirectionChanged!(direction);
+    }
+
+    setState(() {
+      delta = newDelta;
+    });
+  }
+
+  Direction getDirectionFromOffset(Offset offset) {
+    if (offset.dx > 20) {
+      return Direction.right;
+    } else if (offset.dx < -20) {
+      return Direction.left;
+    } else if (offset.dy > 20) {
+      return Direction.down;
+    } else if (offset.dy < -20) {
+      return Direction.up;
+    }
+    return Direction.none;
+  }
+
+  void onDragDown(DragDownDetails d) {
+    calculateDelta(d.localPosition);
+  }
+
+  void onDragUpdate(DragUpdateDetails d) {
+    calculateDelta(d.localPosition);
+  }
+
+  void onDragEnd(DragEndDetails d) {
+    updateDelta(Offset.zero);
+  }
+
+  void calculateDelta(Offset offset) {
+    final newDelta = offset - const Offset(60, 60);
+    updateDelta(
+      Offset.fromDirection(
+        newDelta.direction,
+        min(30, newDelta.distance),
+      ),
+    );
+  }
+}
+
+class WorldCollidable extends PositionComponent {
+  WorldCollidable() {
+    add(RectangleHitbox());
+  }
+}
+
+class MapLoader {
+  static Future<List<Rect>> readRayWorldCollisionMap() async {
+    final collidableRects = <Rect>[];
+    final dynamic collisionMap = json.decode(
+        await rootBundle.loadString('assets/rayworld_collision_map.json'));
+
+    for (final dynamic data in collisionMap['objects']) {
+      collidableRects.add(Rect.fromLTWH(
+          data['x'] as double,
+          data['y'] as double,
+          data['width'] as double,
+          data['height'] as double));
+    }
+
+    return collidableRects;
+  }
+}
